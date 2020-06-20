1023af5d3b91b7ffc7ac9106d65ade4ca50b1eb5
Sandro Baccega <sandro.baccega@smc.it>
2020-06-21 01:45:48 +0200

    :ambulance: Change Gary's password

--- packages/common/src/costants.ts
+++ packages/common/src/costants.ts
@@ -1,4 +1,6 @@
+import { v4 as uuidv4 } from "uuid";
+
 export const INVENTORY_SIZE = 10;
 
 export const GARY_USERNAME = "Gary";
-export const GARY_PASSWORD = "Gary";
+export const GARY_PASSWORD = uuidv4();
